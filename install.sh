599f362caccb0a70044cddb688d6d6fb2f41065d
yuanyangen <yuanyangen@bytedance.com>
2018-10-29 14:13:05 +0800

    add tmux config

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ rm $HOME_DIR/.vimrc
 
 ln -s $CURRENT_DIR/vimrc $HOME_DIR/.vimrc
 ln -s $CURRENT_DIR/zshrc $HOME_DIR/.zshrc
+
+# install tmux config
+cd
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local
